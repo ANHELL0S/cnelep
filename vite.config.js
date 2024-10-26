4778d79443c41359864476e6856f3e81d6fbea66
Angelo García <161162882+ANHELL0S@users.noreply.github.com>
2024-10-26 11:06:23 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 	plugins: [react()],
-	base: '/', // TODO Necesario para poder servir las rutas usando nginx
+	//base: '/', // TODO Necesario para poder servir las rutas usando nginx
 })
